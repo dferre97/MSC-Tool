037ac4e25b761904384403b6dd9e003defda5ab4
dferre1997 <davide.ferre1997@gmail.com>
2022-07-24 21:09:36 +0200

    MSC Tool alpha

--- MSC Tool/my-electron-app/cyclic.js
+++ MSC Tool/my-electron-app/cyclic.js
@@ -1,87 +1,351 @@
-
 // A JavaScript Program to detect cycle in a graph
 
-let V;
-let adj=[];
-function Graph(v)
-{
-    adj=[]; // reset
-	V=v;
-	for (let i = 0; i < V; i++)
-		adj.push([]);
-}
+class DependencyGraph {
+	node_ids = [];
+	highest_id;
+	events;
+	adj = [];
+	proc_edges = [];
+	match_edges = [];
+	hb_edges = [];
+	hb_adj_tc = [];
 
-// This function is a variation of DFSUtil() in
-	// https://www.geeksforgeeks.org/archives/18212
-function isCyclicUtil(i,visited,recStack)
-{
-    console.log("isCyclicUtil with i="+i);
-	// Mark the current node as visited and
+	mb_edges = [];
+	onen_edges = [];
+	nn_edges = [];
+
+	constructor(node_ids, events) {
+		this.node_ids = node_ids;
+		this.highest_id = node_ids[node_ids.length - 1] + 1;
+		this.events = events;
+
+		// computes process relation
+		for (const p_events of this.events) {
+			if (p_events.length > 0) {
+				for (let i = 0; i < p_events.length - 1; i++) {
+					this.proc_edges.push([p_events[i], p_events[i + 1]]);
+				}
+			}
+		}
+
+		// computes matching relation
+		for (const edge of edges) {
+			this.match_edges.push([edge.source, edge.target]);
+		}
+
+		// compute adjacency matrix
+		this.#resetAdjacencyMatrix();
+		this.hb_edges = this.proc_edges.concat(this.match_edges);
+		this.#computeAdjacencyMatrix(this.hb_edges, this.adj);
+	}
+
+	#resetAdjacencyMatrix() {
+		for (let i = 0; i < this.highest_id; i++)
+			this.adj.push([]);
+	}
+	#computeAdjacencyMatrix(edges, adjacency_m) {
+		for (const edge of edges) {
+			this.addEdge(edge[0], edge[1], adjacency_m);
+		}
+	}
+	addEdge(source, dest, adjacency_m) {
+		console.log("addEdge " + source + "," + dest);
+		if (!adjacency_m[source].includes(dest)){
+			adjacency_m[source].push(dest);
+		}
+	}
+
+	isMSC() {
+		return !DependencyGraph.isCyclic(this.adj, this.highest_id);
+	}
+	#procBefore(id1, id2, p) { // id1 and id2 must be on the same process p, returns true if id1 happens before id2
+		if (this.events[p].indexOf(id1) < this.events[p].indexOf(id2))
+			return true;
+		else
+			return false;
+	}
+	ispp() {
+		// check if there are two messages on the same channel where send and receive orders do not match
+		for (let i = 0; i < K; i++) {  // for each process
+			for (let j = 0; j < this.events[i].length - 1; j++) {
+				let id = this.events[i][j];
+				if (id % 2 == 0) { // only send events (even ids)
+					let crt_node = getNodeById(id);
+					let crt_node_rec = getNodeById(id + 1);
+					for (let k = j + 1; k < this.events[i].length; k++) {
+						let tmp_id = this.events[i][k];
+						if (tmp_id % 2 == 0) { // only send events (even ids)
+							let tmp_node = getNodeById(tmp_id);
+							let tmp_node_rec = getNodeById(tmp_id + 1);
+
+							if (crt_node_rec.p == tmp_node_rec.p &&
+								this.#procBefore(tmp_id + 1, id + 1, tmp_node_rec.p)) { // same source, destination, inverted send/receive order
+								return false;
+							}
+						}
+					}
+				}
+			}
+		}
+
+		return true;
+	}
+	#computeHappensBefore() {
+		this.hb_adj_tc = JSON.parse(JSON.stringify(this.adj));
+
+		for (let ev_successors of this.hb_adj_tc) {  // for every event
+			for (let succ of ev_successors) {  // for every adjacent event
+				for (let next of this.hb_adj_tc[succ]) {
+					if (!ev_successors.includes(next))
+						ev_successors.push(next);
+				}
+			}
+		}
+	}
+	#happensBefore(id1, id2) { // check if id1 happens before id2
+		return this.hb_adj_tc[id1].includes(id2);
+	}
+	isco() {
+		this.#computeHappensBefore();
+
+		// check if there are two messages received by the same process, whose send events are causally related, and with inverted send/receive orders
+		for (let i = 0; i < K; i++) {  // for each process
+			for (let j = 0; j < this.events[i].length - 1; j++) {
+				let id = this.events[i][j];
+				if (id % 2 !== 0) { // only receive events (odd ids)
+					for (let k = j + 1; k < this.events[i].length; k++) {
+						let tmp_id = this.events[i][k];
+						if (tmp_id % 2 !== 0) { // only receive events (odd ids)
+							let tmp_node_send = getNodeById(tmp_id - 1);
+
+							if (this.#happensBefore(tmp_id - 1, id - 1, tmp_node_send.p)) { // same destination, inverted send/receive order
+								return false;
+							}
+						}
+					}
+				}
+			}
+		}
+
+		return true;
+	}
+	ismb() {
+		this.mb_edges = [];
+
+		// Find messages received by the same process and impose the mb relation
+		for (let i = 0; i < K; i++) {  // for each process
+			for (let j = 0; j < this.events[i].length - 1; j++) {
+				let id = this.events[i][j];
+				if (id % 2 !== 0) { // only receive events (odd ids)
+					for (let k = j + 1; k < this.events[i].length; k++) {
+						let tmp_id = this.events[i][k];
+						if (tmp_id % 2 !== 0) { // only receive events (odd ids)
+							
+							// add mb relation
+							this.mb_edges.push([id - 1, tmp_id - 1])
+						}
+					}
+				}
+			}
+		}
+
+		let mb_adj = JSON.parse(JSON.stringify(this.adj));
+		for (const edge of this.mb_edges) {
+			this.addEdge(edge[0], edge[1], mb_adj)
+		}
+		
+		return !DependencyGraph.isCyclic(mb_adj, this.highest_id);
+	}
+	isonen() {
+		this.onen_edges = [];
+
+		// Find messages sent by the same process and impose the onen relation
+		for (let i = 0; i < K; i++) {  // for each process
+			for (let j = 0; j < this.events[i].length - 1; j++) {
+				let id = this.events[i][j];
+				if (id % 2 == 0) { // only send events (even ids)
+					for (let k = j + 1; k < this.events[i].length; k++) {
+						let tmp_id = this.events[i][k];
+						if (tmp_id % 2 == 0) { // only send events (even ids)
+							// add onen relation
+							this.onen_edges.push([id + 1, tmp_id + 1])
+						}
+					}
+				}
+			}
+		}
+
+		let onen_adj = JSON.parse(JSON.stringify(this.adj));
+		for (const edge of this.onen_edges) {
+			this.addEdge(edge[0], edge[1], onen_adj)
+		}
+		
+		return !DependencyGraph.isCyclic(onen_adj, this.highest_id);
+	}
+	isnn() {
+		let nn_adj = JSON.parse(JSON.stringify(this.adj));
+		this.nn_edges = this.hb_edges.concat(this.mb_edges).concat(this.onen_edges);
+		for (const edge of this.nn_edges) {
+			this.addEdge(edge[0], edge[1], nn_adj)
+		}
+
+		// compute transitive closure of mb and onen union
+		let nn_adj_tc = JSON.parse(JSON.stringify(nn_adj));
+		for (let ev_successors of nn_adj_tc) {  // for every event
+			for (let succ of ev_successors) {  // for every adjacent event
+				for (let next of nn_adj_tc[succ]) {
+					if (!ev_successors.includes(next))
+						ev_successors.push(next);
+				}
+			}
+		}
+
+		// compute nn rel
+		for(const id1 of this.node_ids) {
+			for (const id2 of this.node_ids) {
+				// case 2 nn relation
+				if (id1 % 2 !== 0 && id1 % 2 !== 0 &&
+					!nn_adj_tc[id1].includes(id2) &&
+					nn_adj_tc[id1-1].includes(id2-1)) { 
+					
+					this.nn_edges.push([id1, id2])
+				}
+				// case 3 nn relation
+				if (id1 % 2 == 0 && id1 % 2 == 0 &&
+					!nn_adj_tc[id1].includes(id2) &&
+					nn_adj_tc[id1+1].includes(id2+1)) { 
+					
+					this.nn_edges.push([id1, id2])
+				}
+			}
+		}
+
+		this.#computeAdjacencyMatrix(this.nn_edges, nn_adj);
+		
+		return !DependencyGraph.isCyclic(nn_adj, this.highest_id);
+	}
+	static isCyclic(adjacency_m, highest_id) {
+		// Mark all the vertices as not visited and
+		// not part of recursion stack
+		let visited = new Array(highest_id);
+		let recStack = new Array(highest_id);
+		for (let i = 0; i < highest_id; i++) {
+			visited[i] = false;
+			recStack[i] = false;
+		}
+
+
+		// Call the recursive helper function to
+		// detect cycle in different DFS trees
+		for (let i = 0; i < highest_id; i++)
+			if (adjacency_m[i] !== null) {
+				if (DependencyGraph.isCyclicUtil(i, visited, recStack, adjacency_m))
+					return true;
+			}
+
+		return false;
+	}
+	static isCyclicUtil(i, visited, recStack, adjacency_m) {
+		// Mark the current node as visited and
 		// part of recursion stack
 		if (recStack[i])
 			return true;
 
 		if (visited[i])
 			return false;
-			
+
 		visited[i] = true;
 
 		recStack[i] = true;
-		let children = adj[i];
+		let children = adjacency_m[i];
 
-		for (let c=0;c< children.length;c++)
-			if (isCyclicUtil(children[c], visited, recStack))
+		for (let c = 0; c < children.length; c++)
+			if (DependencyGraph.isCyclicUtil(children[c], visited, recStack, adjacency_m))
 				return true;
-				
+
 		recStack[i] = false;
 
 		return false;
-}
+	}
 
-function addEdge(source,dest)
-{
-    console.log("addEdge "+source+","+dest);
-	adj[source].push(dest);
-}
+	// other approach based on computing all linearizations and checking their properties
+	analyze_linearizations() {
+		let in_degrees = new Array(this.highest_id).fill(0);
 
-// Returns true if the graph contains a
-	// cycle, else false.
-	// This function is a variation of DFS() in
-	// https://www.geeksforgeeks.org/archives/18212
-function isCyclic()
-{
-	// Mark all the vertices as not visited and
-		// not part of recursion stack
-		let visited = new Array(V);
-		let recStack = new Array(V);
-		for(let i=0;i<V;i++)
-		{
-			visited[i]=false;
-			recStack[i]=false;
+		// compute in-degrees
+		for (let ev_successors of this.adj) {  // for every event
+			for (let succ of ev_successors) {  // for every adjacent event
+				in_degrees[succ]++;
+			}
 		}
-		
-		
-		// Call the recursive helper function to
-		// detect cycle in different DFS trees
-		for (let i = 0; i < V; i++)
-            if (adj[i] !== null) {
-                if (isCyclicUtil(i, visited, recStack))
-				return true;
-            }
 
-		return false;
+		let discovered = new Array(this.highest_id).fill(false);
+		this.findLinearizations(in_degrees, [], discovered);
+	}
+
+	findLinearizations (in_degrees, crt_lin, discovered) {
+		for (const id of this.node_ids) {
+			if (in_degrees[id] == 0 && !discovered[id]) {
+				for (const succ of this.adj[id]) {
+					in_degrees[succ]--;
+				}
+
+				crt_lin.push(id);
+				discovered[id] = true;
+				// console.log("added "+id);
+
+				this.findLinearizations(in_degrees, crt_lin, discovered);
+
+				for (const succ of this.adj[id]) {
+					in_degrees[succ]++;
+				}
+
+				crt_lin.pop();
+				discovered[id] = false;
+				// console.log("removed "+id);
+			}
+		}
+
+		if (crt_lin.length == this.node_ids.length) {
+			console.log(crt_lin);
+
+			if (this.#is_nn_lin(crt_lin)) {
+				console.log("found a nn-linearization !!!");
+				let nn = document.getElementById("nn");
+				nn.innerHTML = "<span style='color: LimeGreen;'><b>YES</b></span>";
+			}
+			if (this.#is_rsc_lin(crt_lin)) {
+				console.log("found a rsc-linearization !!!");
+				let rsc = document.getElementById("rsc");
+				rsc.innerHTML = "<span style='color: LimeGreen;'><b>YES</b></span>";
+			}
+		}
+	}
+
+	#is_nn_lin(lin) {
+		let sends = [];
+		let receives = [];
+
+		for (const ev of lin) {
+			if(ev % 2 == 0)	sends.push(ev);	
+			else receives.push(ev);	
+		}
+
+		for (const i in sends) {
+			if (sends[i] !== receives[i]-1) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+	#is_rsc_lin(lin) {
+		if (lin.length % 2 !== 0) return false;
+
+		for (let i = 0; i < lin.length-1; i+=2) {
+			if (lin[i+1] !== lin[i]+1) return false;
+		}
+
+		return true;
+	}
 }
-
-// // Driver code
-// Graph(4);
-// addEdge(0, 1);
-// addEdge(0, 2);
-// addEdge(1, 2);
-// addEdge(2, 0);
-// addEdge(2, 3);
-
-// if(isCyclic())
-// 	document.write("Graph contains cycle");
-// else
-// 	document.write("Graph doesn't "
-// 				+ "contain cycle");
--- MSC Tool/my-electron-app/msc.js
+++ MSC Tool/my-electron-app/msc.js
@@ -21,112 +21,49 @@ var offsetY = BB.top;
 let events = [];
 let msg_counter = 0;
 let node_ids = [];
-let all_edges = [];
-let proc_edges = [];
-let match_edges = [];
-
-function isMSC() {
-	Graph(node_ids[node_ids.length -1] + 1); // pass highest node id
-	all_edges = proc_edges.concat(match_edges);
-
-	for (const edge of all_edges) {
-		addEdge(edge[0], edge[1]);
-	}
-	
-	if(isCyclic())
-		return false;
-	else
-		return true;
-}
-
-function procBefore(id1, id2, p) { // id1 and id2 must be on the same process p, returns true if id1 happens before id2
-	if (events[p].indexOf(id1) <  events[p].indexOf(id2))
-		return true;
-	else
-		return false;
-}
-function ispp() {
-	// check if there are two messages on the same channel where send and receive orders do not match
-	for (let i = 0; i < K; i++) {  // for each process
-		for (let j=0; j < events[i].length-1; j++) { 
-			let id = events[i][j];
-			if (id % 2 == 0){ // only send events (even ids)
-				let crt_node = getNodeById(id);
-				let crt_node_rec = getNodeById(id+1);
-				for (let k=j+1; k < events[i].length; k++) {
-					let tmp_id = events[i][k];
-					if (tmp_id % 2 == 0) { // only send events (even ids)
-						let tmp_node = getNodeById(tmp_id);
-						let tmp_node_rec = getNodeById(tmp_id+1);
-
-						if(crt_node.p == tmp_node.p && 
-						crt_node_rec.p == tmp_node_rec.p &&
-						procBefore(tmp_id+1, id+1, tmp_node_rec.p)) { // same source, destination, inverted send/receive order
-							return false;
-						}
-					}
-				}
-			}
-		}
-	}
-
-	return true;
-}
+let dep_graph;
 
 function dependency_graph() {
 	node_ids = [];
 	for (const node of nodes) {
 		node_ids.push(node.id);
 	}
-	console.log("node ids: " +node_ids);
+	console.log("node ids: " + node_ids);
 
-	all_edges = [];
-	proc_edges = [];
-	match_edges = [];
+	dep_graph = new DependencyGraph(node_ids, events); // pass highest node id
 
-	// computes process relation
-	for (const p_events of events) {
-		if (p_events.length > 0) {
-			for (let i = 0; i < p_events.length-1; i++) {
-				proc_edges.push([p_events[i], p_events[i+1]]);
-			}
-		}
-	}
+	let asy = document.getElementById("asy"); asy.innerHTML = "<span style='color: RED;'><b>NO</b></span>";
+	let pp = document.getElementById("pp"); pp.innerHTML = "<span style='color: RED;'><b>NO</b></span>";
+	let co = document.getElementById("co"); co.innerHTML = "<span style='color: RED;'><b>NO</b></span>";
+	let mb = document.getElementById("mb"); mb.innerHTML = "<span style='color: RED;'><b>NO</b></span>";
+	let onen = document.getElementById("onen"); onen.innerHTML = "<span style='color: RED;'><b>NO</b></span>";
+	let nn = document.getElementById("nn"); nn.innerHTML = "<span style='color: RED;'><b>NO</b></span>";
+	let rsc = document.getElementById("rsc"); rsc.innerHTML = "<span style='color: RED;'><b>NO</b></span>";
 
-	//computes matching relation
-	for (const edge of edges) {
-		match_edges.push([edge.source, edge.target]);
-	}
+	let is_MSC = dep_graph.isMSC();
+	asy.innerHTML = is_MSC ? "<span style='color: LimeGreen;'><b>YES</b></span>" : "<span style='color: RED;'><b>NO</b></span>"; if (!is_MSC) return;
+	let is_pp = dep_graph.ispp();
+	pp.innerHTML = is_pp ? "<span style='color: LimeGreen;'><b>YES</b></span>" : "<span style='color: RED;'><b>NO</b></span>"; if (!is_pp) return;
+	let is_co = dep_graph.isco();
+	co.innerHTML = is_co ? "<span style='color: LimeGreen;'><b>YES</b></span>" : "<span style='color: RED;'><b>NO</b></span>"; if (!is_co) return;
+	let is_mb = dep_graph.ismb();
+	mb.innerHTML = is_mb ? "<span style='color: LimeGreen;'><b>YES</b></span>" : "<span style='color: RED;'><b>NO</b></span>"; if (!is_mb) return;
+	let is_onen = dep_graph.isonen();
+	onen.innerHTML = is_onen ? "<span style='color: LimeGreen;'><b>YES</b></span>" : "<span style='color: RED;'><b>NO</b></span>"; if (!is_onen) return;
+	// nn and rsc
+	dep_graph.analyze_linearizations();
 
-
-	let asy = document.getElementById("asy"); asy.innerHTML = "NO";
-	let pp = document.getElementById("pp"); pp.innerHTML = "NO";
-	let co = document.getElementById("co"); co.innerHTML = "NO";
-	let mb = document.getElementById("mb"); mb.innerHTML = "NO";
-	let onen = document.getElementById("onen"); onen.innerHTML = "NO";
-	let nn = document.getElementById("nn"); nn.innerHTML = "NO";
-	let rsc = document.getElementById("rsc"); rsc.innerHTML = "NO";
-
-	let is_MSC = isMSC();
-	asy.innerHTML = is_MSC ? "<span style='color: LimeGreen;'><b>YES</b></span>" : "NO"; if (!is_MSC) return;
-	let is_pp = ispp();
-	pp.innerHTML = is_pp ? "<span style='color: LimeGreen;'><b>YES</b></span>" : "NO"; if (!is_pp) return;
-	// let is_co = isco();
-	// co.innerHTML = is_co ? "<span style='color: LimeGreen;'><b>YES</b></span>" : "NO"; if (!is_co) return;
-	// let is_mb = ismb();
-	// mb.innerHTML = is_mb ? "<span style='color: LimeGreen;'><b>YES</b></span>" : "NO"; if (!is_mb) return;
-	// let is_onen = isonen();
-	// onen.innerHTML = is_onen ? "<span style='color: LimeGreen;'><b>YES</b></span>" : "NO"; if (!is_onen) return;
-	// let is_nn = isnn();
-	// nn.innerHTML = is_nn ? "<span style='color: LimeGreen;'><b>YES</b></span>" : "NO"; if (!is_nn) return;
+	// let is_nn = dep_graph.isnn();
+	// nn.innerHTML = is_nn ? "<span style='color: LimeGreen;'><b>YES</b></span>" : "<span style='color: RED;'><b>NO</b></span>"; if (!is_nn) return;
 	// let is_rsc = isrsc();
-	// rsc.innerHTML = is_rsc ? "<span style='color: LimeGreen;'><b>YES</b></span>" : "NO"; if (!is_rsc) return;
+	// rsc.innerHTML = is_rsc ? "<span style='color: LimeGreen;'><b>YES</b></span>" : "<span style='color: RED;'><b>NO</b></span>"; if (!is_rsc) return;
+
 }
 
 function init_events() {
 	for (let i = 0; i < K; i++) { // each row i will be an array of events for the process i
 		events[i] = [];
-	}	
+	}
 }
 
 function reset_data() { nodes = []; edges = []; }
@@ -141,7 +78,7 @@ function init() {
 	R = parseInt(document.getElementById("_ray").value);
 	NODE_COLOR[0] = document.getElementById("_sc").value;
 	NODE_COLOR[1] = document.getElementById("_tc").value;
-	
+
 	ctx.clearRect(0, 0, WIDTH, HEIGHT);
 	ctx.fillStyle = "#fff";
 	ctx.fillRect(0, 0, cnvs.width, cnvs.height);
@@ -162,19 +99,19 @@ function drawCircle(c, x, y, r, col, strk) {
 }
 
 function drawLine(c, xs, ys, xn, yn, col, strk, dash_flag) {
-	if(dash_flag)
+	if (dash_flag)
 		c.setLineDash([5, 10]);
-	
+
 	c.beginPath();
 	c.moveTo(xs, ys);
 	c.lineTo(xn, yn);
 	c.closePath();
-	
+
 	c.strokeStyle = col;
 	c.lineWidth = strk;
 	c.stroke();
-	
-	if(dash_flag)
+
+	if (dash_flag)
 		c.setLineDash([]);
 }
 
@@ -183,15 +120,15 @@ function drawArrow(c, xs, ys, xn, yn, col, strk, dash_flag) {
 	var dx = xn - xs;
 	var dy = yn - ys;
 	var angle = Math.atan2(dy, dx);
-	
+
 	drawLine(c, xs, ys, xn, yn, col, strk, dash_flag);
-	
+
 	c.beginPath();
 	c.moveTo(xn, yn);
 	c.lineTo(xn - headlen * Math.cos(angle - Math.PI / 6), yn - headlen * Math.sin(angle - Math.PI / 6));
 	c.moveTo(xn, yn);
 	c.lineTo(xn - headlen * Math.cos(angle + Math.PI / 6), yn - headlen * Math.sin(angle + Math.PI / 6));
-	
+
 	c.closePath();
 	c.strokeStyle = col;
 	c.lineWidth = strk;
@@ -208,16 +145,16 @@ function getNodeById(id) {
 
 function insert_event(id) {
 	let node = getNodeById(id);
-	console.log("inserting node "+id+" of process "+node.p);
+	console.log("inserting node " + id + " of process " + node.p);
 
 	let p = node.p;
 	let added = false;
 
-	if (events[p].length == 0){
+	if (events[p].length == 0) {
 		console.log("added because empty process list");
 		events[p].push(id);
 		added = true;
-	}	
+	}
 	else {
 		for (let i = 0; i < events[p].length; i++) {
 			if (node.y < getNodeById(events[p][i]).y) {
@@ -225,7 +162,7 @@ function insert_event(id) {
 				console.log("added in correct order");
 				added = true;
 				break;
-			}	
+			}
 		}
 	}
 
@@ -241,7 +178,7 @@ function updateEvents() {
 	console.log("events before inserting");
 	console.log(events);
 
-	for (let i = 0; i < nodes.length; i++) { 
+	for (let i = 0; i < nodes.length; i++) {
 		insert_event(nodes[i].id);
 		// events[nodes[i].p].push(nodes[i].id)
 	}
@@ -250,14 +187,14 @@ function updateEvents() {
 function draw() {
 	console.log("draw() called");
 	init();
-	
-	for(var i = 0; i < K; i++) { drawLine(ctx, (i + 0.5) * (cnvs.width / K), 0, (i + 0.5) * (cnvs.width / K), cnvs.height, "#000", STROKE, false); }
-	
-	for (var i = 0; i < nodes.length; i++) { if(nodes[i] != null) drawLine(ctx, 0, nodes[i].y, cnvs.width, nodes[i].y, "#ddd", STROKE, true); }
-	for (var i = 0; i < nodes.length; i++) { if(nodes[i] != null) drawCircle(ctx, nodes[i].x, nodes[i].y, R, NODE_COLOR[i % 2], STROKE); }
-	
+
+	for (var i = 0; i < K; i++) { drawLine(ctx, (i + 0.5) * (cnvs.width / K), 0, (i + 0.5) * (cnvs.width / K), cnvs.height, "#000", STROKE, false); }
+
+	for (var i = 0; i < nodes.length; i++) { if (nodes[i] != null) drawLine(ctx, 0, nodes[i].y, cnvs.width, nodes[i].y, "#ddd", STROKE, true); }
+	for (var i = 0; i < nodes.length; i++) { if (nodes[i] != null) drawCircle(ctx, nodes[i].x, nodes[i].y, R, NODE_COLOR[i % 2], STROKE); }
+
 	for (var e in edges) {
-		if(edges[e] != null) {
+		if (edges[e] != null) {
 			var src = getNode(nodes, edges[e].source);
 			var trg = getNode(nodes, edges[e].target);
 			drawArrow(ctx, src.x, src.y, trg.x, trg.y, "#000", 1, edges[e].dashed);
@@ -270,8 +207,8 @@ function draw() {
 draw();
 
 function getNode(N, id) {
-	for(var n in N)
-		if(N[n] != null && N[n].id == id)
+	for (var n in N)
+		if (N[n] != null && N[n].id == id)
 			return N[n];
 }
 
@@ -283,7 +220,7 @@ function myDown(e) {
 	// test each rect to see if mouse is inside
 	dragok = false;
 	for (var i = 0; i < nodes.length; i++) {
-		if(nodes[i] != null) {
+		if (nodes[i] != null) {
 			var r = nodes[i];
 			if (mx <= r.x + r.r && mx >= r.x - r.r && my <= r.y + r.r && my >= r.y - r.r) {
 				dragok = true;
@@ -298,71 +235,71 @@ function myDown(e) {
 }
 
 function myUp(e) {
-    e.preventDefault();
-    e.stopPropagation();
+	e.preventDefault();
+	e.stopPropagation();
 
-    // clear all the dragging flags
-    dragok = false;
+	// clear all the dragging flags
+	dragok = false;
 	putNode(e, $(this).position().left, $(this).position().top, false);
-    for (var i = 0; i < nodes.length; i++) { if(nodes[i] != null) nodes[i].isDragging = false; }
+	for (var i = 0; i < nodes.length; i++) { if (nodes[i] != null) nodes[i].isDragging = false; }
 	draw();
 }
 
 function myMove(e) {
-    if (dragok) {
-        e.preventDefault();
-        e.stopPropagation();
+	if (dragok) {
+		e.preventDefault();
+		e.stopPropagation();
 
-        // get the current mouse position
-        var mx = parseInt(e.clientX - offsetX);
-        var my = parseInt(e.clientY - offsetY);
+		// get the current mouse position
+		var mx = parseInt(e.clientX - offsetX);
+		var my = parseInt(e.clientY - offsetY);
 
-        // calculate the distance the mouse has moved
-        // since the last mousemove
-        var dx = mx - startX;
-        var dy = my - startY;
+		// calculate the distance the mouse has moved
+		// since the last mousemove
+		var dx = mx - startX;
+		var dy = my - startY;
 
-        // move each rect that isDragging 
-        // by the distance the mouse has moved
-        // since the last mousemove
-        for (var i = 0; i < nodes.length; i++) {
-			if(nodes[i] != null) {
+		// move each rect that isDragging 
+		// by the distance the mouse has moved
+		// since the last mousemove
+		for (var i = 0; i < nodes.length; i++) {
+			if (nodes[i] != null) {
 				var r = nodes[i];
 				if (r.isDragging) {
 					r.x += dx;
 					r.y += dy;
 				}
 			}
-        }
+		}
 
-        // redraw the scene with the new rect positions
-        draw();
+		// redraw the scene with the new rect positions
+		draw();
 
-        // reset the starting mouse position for the next mousemove
-        startX = mx;
-        startY = my;
+		// reset the starting mouse position for the next mousemove
+		startX = mx;
+		startY = my;
 
-    }
+	}
 }
 
 function putNode(e, lft, tp, newNode) {
 	var posX = e.pageX - lft, posY = e.pageY - tp;
-		
+
 	var x = (Math.round(posX / (cnvs.width / K) - 0.5) + 0.5) * (cnvs.width / K);
 	var y = (Math.round(posY / (cnvs.height / H) - 0.5) + 0.5) * (cnvs.height / H);
 
 
-	console.log(); 
-	
-	
+	console.log();
+
+
 	// drawLine(ctx, 0, y, cnvs.width, y, "#ddd", 1);
 	// drawCircle(ctx, x, y, 25, "#fff", 3);
-	
-	
-	if(newNode) {
+
+
+	if (newNode) {
 		var s_p = Math.abs(Math.round(posX / (cnvs.width / K) - 0.5)); // process number of send
-	
-		if(Math.round(posX / (cnvs.width / K) - 0.5) == K - 1) {
+
+		if (Math.round(posX / (cnvs.width / K) - 0.5) == K - 1) {
 			var xr = x - (cnvs.width / K);
 			var r_p = s_p - 1;
 		}
@@ -371,15 +308,15 @@ function putNode(e, lft, tp, newNode) {
 			var r_p = s_p + 1;
 		}
 		var yr = y;
-		
+
 		msg_counter++;
-		nodes.push({"id": counter, "x": x, "y": y, "r": 15, "p": s_p, "m": msg_counter, "isDragging": false});
-		nodes.push({"id": counter+1, "x": xr, "y": yr, "r": 15, "p": r_p, "m": msg_counter,"isDragging": false});
-		edges.push({"source": counter, "target": counter+1, "dashed": (EDGE_STYLE === "dashed")});
+		nodes.push({ "id": counter, "x": x, "y": y, "r": 15, "p": s_p, "m": msg_counter, "isDragging": false });
+		nodes.push({ "id": counter + 1, "x": xr, "y": yr, "r": 15, "p": r_p, "m": msg_counter, "isDragging": false });
+		edges.push({ "source": counter, "target": counter + 1, "dashed": (EDGE_STYLE === "dashed") });
 		counter += 2;
 	} else {
-		for(var i = 0; i < nodes.length; i++) {
-			if(nodes[i] != null && nodes[i].isDragging) { // update node properties if dragged
+		for (var i = 0; i < nodes.length; i++) {
+			if (nodes[i] != null && nodes[i].isDragging) { // update node properties if dragged
 				nodes[i].x = x;
 				nodes[i].y = y;
 				nodes[i].p = Math.abs(Math.round(posX / (cnvs.width / K) - 0.5));
@@ -390,17 +327,17 @@ function putNode(e, lft, tp, newNode) {
 
 function deleteNode(e, lft, tp) {
 	var posX = e.pageX - lft, posY = e.pageY - tp;
-		
+
 	var x = (Math.round(posX / (cnvs.width / K) - 0.5) + 0.5) * (cnvs.width / K);
 	var y = (Math.round(posY / (cnvs.height / H) - 0.5) + 0.5) * (cnvs.height / H);
-	
-	
+
+
 	// drawLine(ctx, 0, y, cnvs.width, y, "#ddd", 1);
 	// drawCircle(ctx, x, y, 25, "#fff", 3);
-	
-	for(var i = 0; i < nodes.length; i++) {
-		if(nodes[i] != null && nodes[i].x == x && nodes[i].y == y) {
-			var j = Math.floor(i / 2) * 2, y = j+1;
+
+	for (var i = 0; i < nodes.length; i++) {
+		if (nodes[i] != null && nodes[i].x == x && nodes[i].y == y) {
+			var j = Math.floor(i / 2) * 2, y = j + 1;
 			// delete nodes[y];
 			// delete nodes[j];
 			// delete edges[j / 2];
@@ -409,20 +346,20 @@ function deleteNode(e, lft, tp) {
 			nodes.splice(y, 1);
 			nodes.splice(j, 1);
 			edges.splice(j / 2, 1);
-			
+
 			break;
 		}
 	}
 }
 
 $(document).ready(function () {
-	
+
 	$('#cnvs').mousedown(function (e) {
 		e.preventDefault();
 		if (e.which == 1) { // left click
 			myDown(e);
 		} else {
-			if(MODE === "adding")
+			if (MODE === "adding")
 				putNode(e, $(this).position().left, $(this).position().top, true);
 			else
 				deleteNode(e, $(this).position().left, $(this).position().top);
@@ -431,6 +368,6 @@ $(document).ready(function () {
 	});
 	$('#cnvs').mouseup(myUp);
 	$('#cnvs').mousemove(myMove);
-	
-	$(document).on('contextmenu', function(e) { e.preventDefault(); });
+
+	$(document).on('contextmenu', function (e) { e.preventDefault(); });
 });
